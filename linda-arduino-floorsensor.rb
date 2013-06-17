c6b2f6333340d381199dd0d2196d6b2d45b69f30
Sho Hashimoto <hashimoto@shokai.org>
2013-06-17 15:47:37 +0900

    use eventmachine and em-rocketio-linda-client

--- linda-arduino-floorsensor.rb
+++ linda-arduino-floorsensor.rb
@@ -1,29 +1,30 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'sinatra/rocketio/linda/client'
+require 'em-rocketio-linda-client'
 require 'arduino_firmata'
 $stdout.sync = true
 
-arduino = ArduinoFirmata.connect ENV["ARDUINO"]
-puts "firmata board version v#{arduino.version}"
+EM::run do
+  arduino = ArduinoFirmata.connect ENV["ARDUINO"], :eventmachine => true
+  puts "firmata board version v#{arduino.version}"
 
-url   = ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
-space = ENV["LINDA_SPACE"] || "test"
-puts "connecting.. #{url}"
-linda = Sinatra::RocketIO::Linda::Client.new url
-ts = linda.tuplespace[space]
+  url   = ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
+  space = ENV["LINDA_SPACE"] || "test"
+  puts "connecting.. #{url}"
+  linda = EM::RocketIO::Linda::Client.new url
+  ts = linda.tuplespace[space]
 
-linda.io.on :connect do  ## RocketIO's "connect" event
-  puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
-end
+  linda.io.on :connect do  ## RocketIO's "connect" event
+    puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
+  end
 
-linda.io.on :disconnect do
-  puts "RocketIO disconnected.."
-end
+  linda.io.on :disconnect do
+    puts "RocketIO disconnected.."
+  end
 
-loop do
-  floor = arduino.analog_read 5
-  puts "floor : #{floor}"
-  ts.write ["sensor", "floor", floor]
-  sleep 0.1
+  EM::add_periodic_timer 0.3 do
+    floor = arduino.analog_read 5
+    puts "floor : #{floor}"
+    ts.write ["sensor", "floor", floor]
+  end
 end
